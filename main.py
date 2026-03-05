15a96ab8ecc4ffd1ee07995a245cbb673f0c1d09
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:00:46 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:00:38.154Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:00:44.959Z
+Commit: Fix CSS styling bug on mobile devices
