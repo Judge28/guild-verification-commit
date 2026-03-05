b5dbdba8e86e0c7f5ed2f867342ab036a60220a1
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:03:04 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:02:56.291Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T16:03:02.841Z
+Commit: Fix broken links in documentation
