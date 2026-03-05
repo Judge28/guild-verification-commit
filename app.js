00f896c990eeb86bbcdc26d0a421e614148115a8
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 18:59:29 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:59:22.114Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T15:59:27.950Z
+Commit: Resolve null pointer exception in user service
