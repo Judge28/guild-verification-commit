4af7d6a35b225c8bfae735b636b08347187757a4
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:00:18 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:00:09.178Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T16:00:17.476Z
+Commit: Fix CSS styling bug on mobile devices
