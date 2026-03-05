05494afa5329902ece21d4065f60dace2b6dc913
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:00:02 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:59:58.344Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T16:00:00.806Z
+Commit: Fix race condition in async operations
