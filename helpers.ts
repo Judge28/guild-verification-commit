1ad88e402a76c3f8fc20c8a2085615d717a08796
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 18:59:26 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:59:18.707Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T15:59:25.116Z
+Commit: Correct typo in configuration file
