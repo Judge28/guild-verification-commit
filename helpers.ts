4b2191852c8da9767071ce0e0b6b52477904c97d
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:00:25 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:00:21.888Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T16:00:24.314Z
+Commit: Resolve null pointer exception in user service
