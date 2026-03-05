350dde462edd7cc0e6b1b5e672c29693f3a58bac
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:00:07 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:59:55.994Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:00:06.652Z
+Commit: Add multi-language support
