f27966978b49394771e799f6fc3a27d87de2cc23
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:02:12 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:00:49.388Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:02:11.036Z
+Commit: Fix memory leak in event handlers
