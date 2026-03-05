c34b6aae505630eab90cc235aa91335a5eddcba2
Judge28 <sariyildizzz10@gmail.com>
2026-03-05 19:06:21 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:02:11.036Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T16:06:20.286Z
+Commit: Fix CSS styling bug on mobile devices
